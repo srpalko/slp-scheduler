728c089c0dfb0ae22231d948560539d8c33b7215
Steve Palko <steve_palko@outlook.com>
2021-06-15 15:19:47 -0400

    Initial commit

---
+++ slps/src/main/java/org/steve/palko/slps/SlpsApplication.java
@@ -0,0 +1,13 @@
+package org.steve.palko.slps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlpsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SlpsApplication.class, args);
+    }
+
+}
---
+++ slps/src/test/java/org/steve/palko/slps/SlpsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.steve.palko.slps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlpsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
